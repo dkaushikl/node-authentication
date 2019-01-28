827c35c318ec52f44160d6724bd8a0929b76a22a
Paras Bhesaniya <paras.bhesaniya@gmail.com>
2019-01-28 17:58:12 +0530

    Add Procfile and update portno

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ app.use((err, req, res, next) => {
         });
 })
 
-app.listen(3000, function () {
-    console.log('Node server listening on port 3000');
+app.listen(80, function () {
+    console.log('Node server listening on port 80');
 });
